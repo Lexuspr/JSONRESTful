84058e85dca831c6b11f7e5f6e821cadd4ad11d2
Jesus <jesus.palo@tecsup.edu.pe>
2018-06-11 14:42:14 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by MAC10 on 11/06/18.
+//  Copyright © 2018 tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
